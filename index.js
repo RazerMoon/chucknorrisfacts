322a2bb3d26a81f21ccbedc36f29c8e1cad5d6ae
Rasync <gamespeterplelit@gmail.com>
2021-03-27 20:00:16 +0000

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Plugin } = require('powercord/entities');
+const { get } = require('powercord/http');
+const { open } = require('powercord/modal');
+const { React } = require('powercord/webpack');
+const JokeModal = require("./jokeModal")
+
+module.exports = class ChuckNorrisFacts extends Plugin {
+  startPlugin () {
+    this.loadStylesheet("styles.css")
+    get("https://api.chucknorris.io/jokes/random").execute().then((result) => this.showJoke(result.body)).catch((err) => this.warn(err))
+  }
+
+  showJoke (joke) {
+    powercord.api.notices.sendAnnouncement('rz-chucknorrisfacts', {
+      message: "Found a Chuck Norris fact!",
+      button: {
+        onClick: () => {open(() => React.createElement(JokeModal, {joke}))},
+        text: "Show"
+      }
+    });
+  }
+};
---
+++ jokeModal/index.jsx
@@ -0,0 +1,33 @@
+const { React } = require("powercord/webpack");
+const { Button } = require("powercord/components");
+const { Modal } = require("powercord/components/modal");
+const { close: closeModal } = require("powercord/modal");
+
+module.exports = class JokeModal extends React.PureComponent {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <Modal className="powercord-text rz-modal-top">
+        <h1 className="title">A Chuck Norris fact was found!</h1>
+        <Modal.Content>
+          <p>{this.props.joke.value}</p>
+        </Modal.Content>
+        <Modal.Footer>
+          <Button onClick={() => closeModal()}>Thanks</Button>
+          <a href={`${this.props.joke.url}`} target="_blank">
+            <Button
+              onClick={() => closeModal()}
+              look={Button.Looks.LINK}
+              color={Button.Colors.TRANSPARENT}
+            >
+              Source
+            </Button>
+          </a>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+};
