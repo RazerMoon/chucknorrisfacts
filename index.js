35767ada3a18d13ac17e9cc6faaa8b049bca73c6
Rasync <gamespeterplelit@gmail.com>
2021-03-28 23:44:19 +0100

    Adhere to powercord eslint

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-const { Plugin } = require("powercord/entities");
-const { get } = require("powercord/http");
-const { open } = require("powercord/modal");
-const { React } = require("powercord/webpack");
-const JokeModal = require("./jokeModal")
+const { Plugin } = require('powercord/entities');
+const { get } = require('powercord/http');
+const { open } = require('powercord/modal');
+const { React } = require('powercord/webpack');
+const JokeModal = require('./jokeModal');
 
 module.exports = class ChuckNorrisFacts extends Plugin {
-  run = () => get("https://api.chucknorris.io/jokes/random").execute().then((result) => this.showJoke(result.body)).catch((err) => this.warn(err));
-
   startPlugin () {
-    this.loadStylesheet("styles.css");
+    this.run = () => get('https://api.chucknorris.io/jokes/random').execute().then((result) => this.showJoke(result.body)).catch((err) => this.warn(err));
+    this.loadStylesheet('styles.css');
     this.setupCommand();
-    this.run()
+    this.run();
   }
 
   showJoke (joke) {
-    powercord.api.notices.sendAnnouncement("chucknorrisfacts", {
-      message: "Found a Chuck Norris fact!",
+    powercord.api.notices.sendAnnouncement('chucknorrisfacts', {
+      message: 'Found a Chuck Norris fact!',
       button: {
-        onClick: () => {open(() => React.createElement(JokeModal, {joke}))},
-        text: "Show"
+        onClick: () => {
+          open(() => React.createElement(JokeModal, { joke }));
+        },
+        text: 'Show'
       }
     });
   }
 
   setupCommand () {
     powercord.api.commands.registerCommand({
-      command: "chuckfact",
-      description: "Displays a Chuck Norris fact",
-      usage: "{c}",
+      command: 'chuckfact',
+      description: 'Displays a Chuck Norris fact',
+      usage: '{c}',
       executor: () => this.run()
     });
   }
 
   pluginWillUnload () {
-    powercord.api.commands.unregisterCommand("chuckfact");
+    powercord.api.commands.unregisterCommand('chuckfact');
   }
 };
--- jokeModal/index.jsx
+++ jokeModal/index.jsx
@@ -1,14 +1,10 @@
-const { React } = require("powercord/webpack");
-const { Button } = require("powercord/components");
-const { Modal } = require("powercord/components/modal");
-const { close: closeModal } = require("powercord/modal");
+const { React } = require('powercord/webpack');
+const { Button } = require('powercord/components');
+const { Modal } = require('powercord/components/modal');
+const { close: closeModal } = require('powercord/modal');
 
 module.exports = class JokeModal extends React.PureComponent {
-  constructor() {
-    super();
-  }
-
-  render() {
+  render () {
     return (
       <Modal className="powercord-text rz-modal-top">
         <h1 className="title">A Chuck Norris fact was found!</h1>
